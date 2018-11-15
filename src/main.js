da2178dc144d822f5849152c8fd8fd336fc287dc
admin <wangjunhui@nequal.com>
2018-11-15 17:21:11 +0800

    vue-wechat-title

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import config from './config';
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import VueWechatTitle from 'vue-wechat-title';
 
+Vue.use(VueWechatTitle)
 Vue.use(iView)
 Vue.prototype.$config = config
 Vue.config.productionTip = false
